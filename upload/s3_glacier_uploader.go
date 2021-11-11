509d34929466703240fc2c3a4a0e34c6dbeebd0d
xinixgit <rutgers.scarlet@gmail.com>
2021-11-10 16:48:28 -0800

    refactor out db_helper for uploader

--- upload/s3_glacier_uploader.go
+++ upload/s3_glacier_uploader.go
@@ -3,14 +3,13 @@ package upload
 import (
 	"os"
 	"fmt"
-	"time"
 	"bytes"
 	"strconv"
 	"encoding/hex"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/glacier"
-	"xddd/s3glacier/util"
 	"xddd/s3glacier/db"
+	"xddd/s3glacier/util"
 )
 
 var ONE_MB = 1024 * 1024
@@ -33,18 +32,18 @@ func (u S3GlacierUploader) Upload(filePath string) {
 	fl := int(util.FileLen(f))
 	fmt.Printf("Now start uploading file %s of %d bytes.\n", filePath, fl)
 
-	uploadSessionId := u.InitiateMultipartUpload()
-	id := u.InsertNewUpload(uploadSessionId, filePath)
+	uploadSessionId := u.initiateMultipartUpload()
+	id := insertNewUpload(uploadSessionId, filePath, &u)
 
-	finalChecksum := u.UploadSegments(uploadSessionId, filePath, fl, id, f)
+	finalChecksum := u.uploadSegments(uploadSessionId, filePath, fl, id, f)
 
-	result := u.CompleteMultipartUpload(fl, finalChecksum, uploadSessionId)
-	u.UpdateCompletedUpload(id, result)
+	result := u.completeMultipartUpload(fl, finalChecksum, uploadSessionId)
+	updateCompletedUpload(id, result, &u)
 
 	fmt.Println("File uploaded, result: ", result)
 }
 
-func (u S3GlacierUploader) InitiateMultipartUpload() *string {
+func (u S3GlacierUploader) initiateMultipartUpload() *string {
 	input := &glacier.InitiateMultipartUploadInput{
     AccountId: 					aws.String("-"),
     PartSize:  					aws.String(strconv.Itoa(PART_SIZE)),
@@ -62,7 +61,7 @@ func (u S3GlacierUploader) InitiateMultipartUpload() *string {
 }
 
 
-func (u S3GlacierUploader) UploadSegments(uploadSessionId *string, filePath string, fl int, uploadId uint, f *os.File) *string {
+func (u S3GlacierUploader) uploadSegments(uploadSessionId *string, filePath string, fl int, uploadId uint, f *os.File) *string {
 	off := 0
 	buf := make([]byte, PART_SIZE)
 	hashes := [][]byte{}
@@ -92,12 +91,13 @@ func (u S3GlacierUploader) UploadSegments(uploadSessionId *string, filePath stri
 		result, err := u.S3glacier.UploadMultipartPart(input)
 		if err != nil {
 			fmt.Printf("(%d/%d) failed for file %s.\n", segNum, segCount, filePath)
-			u.AbortMultipartUpload(uploadSessionId)
+			u.abortMultipartUpload(uploadSessionId)
+			updateFailedUpload(uploadId, &u)
 			panic(err)
 		}
 
 		fmt.Printf("(%d/%d) uploaded for upload %d.\n", segNum, segCount, uploadId)
-		u.InsertUploadedSegment(result, segNum, segCount, uploadId)
+		insertUploadedSegment(result, segNum, segCount, uploadId, &u)
 
 		segNum = segNum + 1
 		off = off + read
@@ -107,7 +107,7 @@ func (u S3GlacierUploader) UploadSegments(uploadSessionId *string, filePath stri
 	return &encoded
 }
 
-func (u S3GlacierUploader) AbortMultipartUpload(uploadSessionId *string) {
+func (u S3GlacierUploader) abortMultipartUpload(uploadSessionId *string) {
 	input := &glacier.AbortMultipartUploadInput{
 		AccountId:  	aws.String("-"),
 		UploadId:   	uploadSessionId,
@@ -121,25 +121,7 @@ func (u S3GlacierUploader) AbortMultipartUpload(uploadSessionId *string) {
 	}
 }
 
-func (u S3GlacierUploader) InsertUploadedSegment(result *glacier.UploadMultipartPartOutput, segNum int, segCount int, uploadId uint) {
-	if uploadId == 0 {
-		return
-	}
-
-	seg := &db.UploadedSegment{
-		SegmentNum:  	segNum,
-		UploadId: 		uploadId,
-		Checksum: 		*result.Checksum,
-		CreatedAt:  	time.Now().Format("2006-01-02 15:04:05"),
-	}
-
-	err := u.DBDAO.InsertUploadedSegment(seg)
-	if err != nil {
-		fmt.Printf("Insert uploaded segment failed for seg num %d, and upload id %d.\n", segNum, uploadId)
-	}
-}
-
-func (u S3GlacierUploader) CompleteMultipartUpload(fl int, checksum *string, uploadSessionId *string) *glacier.ArchiveCreationOutput {
+func (u S3GlacierUploader) completeMultipartUpload(fl int, checksum *string, uploadSessionId *string) *glacier.ArchiveCreationOutput {
 	input := &glacier.CompleteMultipartUploadInput{
     AccountId:   aws.String("-"),
     ArchiveSize: aws.String(strconv.Itoa(fl)),
@@ -152,34 +134,4 @@ func (u S3GlacierUploader) CompleteMultipartUpload(fl int, checksum *string, upl
 		panic(err)
 	}
 	return res
-}
-
-func (u S3GlacierUploader) InsertNewUpload(sessionId *string, filename string) uint {
-	upload := &db.Upload{
-		VaultName: 	*u.Vault,
-		Filename:		filename,
-		SessionId:	*sessionId,
-		CreatedAt:  time.Now().Format("2006-01-02 15:04:05"),
-		Status:  		db.STARTED,
-	}
-
-	err := u.DBDAO.InsertUpload(upload)
-	if err != nil {
-		fmt.Printf("Insert upload failed for %s and session %s.\n", *sessionId, filename)
-	}
-	return upload.ID
-}
-
-func (u S3GlacierUploader) UpdateCompletedUpload(id uint, res *glacier.ArchiveCreationOutput) {
-	upload := u.DBDAO.GetUploadByID(id)
-	if upload == nil {
-		fmt.Printf("Failed to update upload %d: record not found.\n", id)
-		return
-	}
-
-	upload.Location 	= *res.Location
-	upload.Checksum 	= *res.Checksum
-	upload.ArchiveId 	= *res.ArchiveId
-	upload.Status 		= db.COMPLETED
-	u.DBDAO.UpdateUpload(upload)
 }
---
+++ upload/uploader_db_helper.go
@@ -0,0 +1,67 @@
+package upload
+
+import (
+	"fmt"
+	"time"
+	"github.com/aws/aws-sdk-go/service/glacier"
+	"xddd/s3glacier/db"
+)
+
+func insertNewUpload(sessionId *string, filename string, u *S3GlacierUploader) uint {
+	upload := &db.Upload{
+		VaultName: 	*u.Vault,
+		Filename:		filename,
+		SessionId:	*sessionId,
+		CreatedAt:  time.Now().Format("2006-01-02 15:04:05"),
+		Status:  		db.STARTED,
+	}
+
+	err := u.DBDAO.InsertUpload(upload)
+	if err != nil {
+		fmt.Printf("Insert upload failed for %s and session %s.\n", *sessionId, filename)
+	}
+	return upload.ID
+}
+
+func updateCompletedUpload(id uint, res *glacier.ArchiveCreationOutput, u *S3GlacierUploader) {
+	upload := u.DBDAO.GetUploadByID(id)
+	if upload == nil {
+		fmt.Printf("Failed to update upload %d: record not found.\n", id)
+		return
+	}
+
+	upload.Location 	= *res.Location
+	upload.Checksum 	= *res.Checksum
+	upload.ArchiveId 	= *res.ArchiveId
+	upload.Status 		= db.COMPLETED
+	u.DBDAO.UpdateUpload(upload)
+}
+
+func updateFailedUpload(id uint, u *S3GlacierUploader) {
+	upload := u.DBDAO.GetUploadByID(id)
+	if upload == nil {
+		fmt.Printf("Failed to update upload %d: record not found.\n", id)
+		return
+	}
+
+	upload.Status = db.FAILED
+	u.DBDAO.UpdateUpload(upload)
+}
+
+func insertUploadedSegment(result *glacier.UploadMultipartPartOutput, segNum int, segCount int, uploadId uint, u *S3GlacierUploader) {
+	if uploadId == 0 {
+		return
+	}
+
+	seg := &db.UploadedSegment{
+		SegmentNum:  	segNum,
+		UploadId: 		uploadId,
+		Checksum: 		*result.Checksum,
+		CreatedAt:  	time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	err := u.DBDAO.InsertUploadedSegment(seg)
+	if err != nil {
+		fmt.Printf("Insert uploaded segment failed for seg num %d, and upload id %d.\n", segNum, uploadId)
+	}
+}
